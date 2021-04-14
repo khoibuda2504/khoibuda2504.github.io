8100b50a0eba08525a98d0fa8fe92d3db0814eb9
khoibuda2504 <68694998+khoibuda2504@users.noreply.github.com>
2021-04-14 10:06:01 +0700

    forEach

--- ProjectJS/MediumProjects/form-validation/main.js
+++ ProjectJS/MediumProjects/form-validation/main.js
@@ -71,29 +71,38 @@ function setSuccessFor(input) {
 function isEmail(email) {
     return /^[a-z][a-z0-9_\.]{5,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}$/gm.test(email)
 }
+const arrs = [username,email,password,password2];
 
-password2.addEventListener('focus', () => {
-    const parent = password2.parentElement;
-    if(parent.classList.contains('error')) {
-        parent.classList.remove('error')
-    }
+arrs.forEach(arr => {
+    arr.addEventListener('focus', () => {
+        const parent = arr.parentElement;
+        if(parent.classList.contains('error')) {
+            parent.classList.remove('error')
+        }
+    })
 })
-username.addEventListener('focus', () => {
-    const parent = username.parentElement;
-    if(parent.classList.contains('error')) {
-        parent.classList.remove('error')
-    }
-})
-email.addEventListener('focus', () => {
-    const parent = email.parentElement;
-    console.log(parent.classList);
-    if(parent.classList.contains('error')) {
-        parent.classList.remove('error')
-    }
-})
-password.addEventListener('focus', () => {
-    const parent = password.parentElement;
-    if(parent.classList.contains('error')) {
-        parent.classList.remove('error')
-    }
-})
+// password2.addEventListener('focus', () => {
+//     const parent = password2.parentElement;
+//     if(parent.classList.contains('error')) {
+//         parent.classList.remove('error')
+//     }
+// })
+// username.addEventListener('focus', () => {
+//     const parent = username.parentElement;
+//     if(parent.classList.contains('error')) {
+//         parent.classList.remove('error')
+//     }
+// })
+// email.addEventListener('focus', () => {
+//     const parent = email.parentElement;
+//     console.log(parent.classList);
+//     if(parent.classList.contains('error')) {
+//         parent.classList.remove('error')
+//     }
+// })
+// password.addEventListener('focus', () => {
+//     const parent = password.parentElement;
+//     if(parent.classList.contains('error')) {
+//         parent.classList.remove('error')
+//     }
+// })
